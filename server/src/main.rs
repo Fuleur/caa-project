2239aebed86dd19cb34a2598b9b152f0b3569bf2
Fuler <dorian.niclass@gmail.com>
2023-12-01 20:02:39 +0100

    Add ServerSetup generation with --setup arg

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,9 @@
 use axum::{routing::post, Extension, Router};
 use base64::{engine::general_purpose, Engine as _};
+use colored::Colorize;
 use dotenv::dotenv;
 use opaque_ke::{ServerLoginStartResult, ServerSetup};
+use rand::rngs::OsRng;
 use routes::auth::{self, DefaultCS};
 use std::{
     collections::HashMap,
@@ -12,12 +14,28 @@ use std::{
 mod log;
 mod routes;
 
-const LISTENING_ADDRESS: &str = "0.0.0.0";
-const PORT: u16 = 1315;
-
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
+    // If --setup arg is passed, generate a fresh ServerSetup and print it's base64 serialization
+    if env::args().find(|a| a == "--setup").is_some() {
+        println!("Generating a fresh ServerSetup. Use it in your OPAQUE_SERVER_SETUP env var.\n");
+        let mut rng = OsRng;
+        let server_setup = ServerSetup::<DefaultCS>::new(&mut rng);
+        let b64_server_setup = general_purpose::STANDARD_NO_PAD.encode(server_setup.serialize());
+        println!("{}: {}", "OPAQUE ServerSetup".cyan(), b64_server_setup);
+
+        return;
+    }
+
+    // Loading env variables
+    let opaque_server_setup =
+        env::var("OPAQUE_SERVER_SETUP").expect("Missing `OPAQUE_SERVER_SETUP` env variable");
+    let listening_address =
+        env::var("LISTENING_ADDRESS").expect("Missing `LISTENING_ADDRESS` env variable");
+    let port = env::var("PORT").expect("Missing `PORT` env variable");
+    let redis_url = env::var("REDIS_URL").expect("Missing `REDIS_URL` env variable");
 
     // Get the ServerSetup from env
     // Using a saved ServerSetup is needed to have persistence
@@ -29,7 +47,7 @@ async fn main() {
     //      let b64_server_setup = general_purpose::STANDARD_NO_PAD.encode(server_setup.serialize());
     //      println!("{}", b64_server_setup); <--- Put this in OPAQUE_SERVER_SETUP env var
     let server_setup_serialized = general_purpose::STANDARD_NO_PAD
-        .decode(env::var("OPAQUE_SERVER_SETUP").expect("Missing OPAQUE_SERVER_SETUP in env file"))
+        .decode(opaque_server_setup)
         .unwrap();
 
     // Deserialize the ServerSetup
@@ -39,7 +57,7 @@ async fn main() {
 
     let app_state = Arc::new(RwLock::new(AppState {
         server_login_states: HashMap::<String, ServerLoginStartResult<DefaultCS>>::new(),
-        redis_client: redis::Client::open("redis://127.0.0.1/").unwrap(),
+        redis_client: redis::Client::open(redis_url).unwrap(),
     }));
 
     // Initilize Axum app
@@ -51,13 +69,13 @@ async fn main() {
         .layer(Extension(server_setup_state))
         .with_state(app_state);
 
-    let listener = tokio::net::TcpListener::bind(format!("{}:{}", LISTENING_ADDRESS, PORT))
+    let listener = tokio::net::TcpListener::bind(format!("{}:{}", listening_address, port))
         .await
         .unwrap();
 
     log::info(&format!(
         "Server listening on {}:{}",
-        LISTENING_ADDRESS, PORT
+        listening_address, port
     ));
 
     axum::serve(listener, app).await.unwrap();
