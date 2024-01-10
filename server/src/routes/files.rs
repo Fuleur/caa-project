47684d8edb15a1f4bfb5c0ccaeb02887d1ac9569
Fuler <dorian.niclass@gmail.com>
2024-01-10 17:31:24 +0100

    .

--- server/src/routes/files.rs
+++ server/src/routes/files.rs
@@ -26,6 +26,14 @@ pub struct UploadFileResponse {
     keyring: KeyringWithKeys,
 }
 
+/// Allow a user to upload a file.
+/// 
+/// The file uploaded is encrypted and his encrypted symmetric encryption key
+/// is send along with it. The file symmetric key is encrypted with the user's public key.
+/// The file will be "placed" in the specified path starting from the user's root.
+/// 
+/// If the specified path doesn't exist, return an error
+/// Else return a response with the updated user root keyring
 pub async fn upload_file(
     Extension(user_session): Extension<Session>,
     State(app_state): State<AppState>,
@@ -101,8 +109,25 @@ pub async fn upload_file(
     })
 }
 
+/// Allow a user to download a file.
+/// 
+/// The file should not be sent to the user if he has no access to it.
+/// The path specified is the path starting from the user's root, by following the path
+/// from the user's root keyring we can check if it exist or not. Each user has his own file hierarchy.
+/// 
+/// Note: the path is not a path by name, but a path by uuid. The client application transform
+/// the path input from the user to files uuid using the informations in the keyring chain.
 pub async fn download_file() {}
 
+/// Allow a user to delete a file
 pub async fn delete_file() {}
 
+/// Allow a use to share a file with another user
+/// 
+/// Receive the file key encrypted with the destination user public key from the client
+/// push this key in the destination user root keychain.
+/// 
+/// If it's a file, then the destination user will have access to this file from his root.
+/// If it's a folder, then the destination user will have access to this folder
+/// and all subsequent files/folder from his root.
 pub async fn share_file() {}
